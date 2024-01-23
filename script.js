a6549a59bc50f8e820213aa44714ef2a34c830a6
karimrustamov <karimrustamov@hotmail.de>
2024-01-23 18:28:02 +0100

    23.01.24

--- script.js
+++ script.js
@@ -1,22 +1,38 @@
 let currentPokemon;
+let pokemonID = 1; // Start from the first Pokemon
 
-async function loadPokemon() {
-    let url = 'https://pokeapi.co/api/v2/pokemon/pikachu';
-    let response = await fetch(url);
-    currentPokemon = await response.json();
-
-    console.log('Loaded Pokemon:', currentPokemon)
-
-    renderPokemonInfo();
+async function init() {
+    while (pokemonID <= 151) {
+        await loadPokemon();
+        pokemonID++;
+    }
 }
 
-//function renderPokemonInfo () {
-//    document.getElementById('pokemonName').innerHTML = currentPokemon['name'];
-//}
+async function loadPokemon() {
+    try {
+        let url = 'https://pokeapi.co/api/v2/pokemon/' + pokemonID;
+        let response = await fetch(url);
+        currentPokemon = await response.json();
+        renderPokemonInfo();
+    } catch (error) {
+        console.error('Error loading Pokemon:', error);
+    }
+}
 
 function renderPokemonInfo() {
-    let name = currentPokemon['name']; // Hole den Namen
-    let capitalizedName = name.charAt(0).toUpperCase() + name.slice(1); // Erste Buchstabe großschreiben und Rest beibehalten
-    document.getElementById('pokemonName').innerHTML = capitalizedName; // Setze den Namen in das HTML-Element
-    document.getElementById('pokemonImage').src = currentPokemon['sprites']['front_default']
+    // Create a new card for each Pokemon
+    let card = document.createElement('div');
+    card.className = 'pokemonCard';
+
+    let name = currentPokemon['name'];
+    let capitalizedName = name.charAt(0).toUpperCase() + name.slice(1);
+    
+    card.innerHTML = `
+        <h1>${capitalizedName}</h1>
+        <img src="${currentPokemon['sprites']['front_default']}" alt="${capitalizedName}">
+        <div>#${currentPokemon['id']}</div>
+        <div>${currentPokemon['types'].map(type => type['type']['name']).join(', ')}</div>
+    `;
+
+    document.getElementById('allCards').appendChild(card);
 }
